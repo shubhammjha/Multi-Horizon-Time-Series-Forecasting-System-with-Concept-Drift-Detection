62e5119005608848008068627072fb15ae702783
Shubham Jha <81228090+shubhammjha@users.noreply.github.com>
2026-01-12 08:29:33 +0530

    added path.py to access the data parquet

---
+++ config/paths.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+BASE_DIR = Path(
+    r"C:\Jupyter Notebook\PROJECTS\Multi_Horizon_Time_Series_Forecasting_System_with_Concept_Drift_Detection"
+)
+
+DATA_DIR = BASE_DIR / "data" / "processed"
